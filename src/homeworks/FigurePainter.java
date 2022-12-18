9576a8ea88a1a704ad5d1bdcfba06320d5bd7c23
GevorAvoyan <gevorg.avoyan.2019@gmail.com>
2022-12-18 09:07:56 +0400

    homework1

---
+++ src/homeworks/FigurePainter.java
@@ -0,0 +1,37 @@
+package homeworks;
+
+//Figure 1
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+
+        //figure 3
+        for (int i = 1; i < 6; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/homeworks/IfForExample.java
@@ -0,0 +1,28 @@
+package homeworks;
+
+//homework 1
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+        if (a < b) {
+            System.out.println("max: 40");
+        }
+
+        //homework 2
+        int y = 11;
+        for (int x = 5; x < y; x++) {
+            System.out.print(x + ",");
+        }
+
+        //homework 4
+        int m = 10;
+        int n = 5;
+        if (m == n) {
+            System.out.print("m-ն հավասար է n-ին");
+        } else {
+            System.out.println("\nհավասար չէ");
+        }
+    }
+
+}
